8df3e26bf216c46aaa42c7b204146d6eb5693810
chizu praise <chizupraise31@gmail.com>
2024-07-09 03:37:22 -0700

    added loading state

--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,5 +1,254 @@
 // src/components/Home.js
 
+// import React, { useEffect, useState } from "react";
+// import { Navigate, useNavigate } from "react-router-dom";
+// import "../../App.css";
+// import { Call, StreamVideo } from "@stream-io/video-react-sdk";
+// import { useUser } from "../../user-context";
+// import CryptoJS from "crypto-js";
+// import { BsPeopleFill } from "react-icons/bs";
+
+// interface Room {
+//   id: string;
+//   title: string;
+//   description: string;
+//   participantsLength: number;
+//   createdBy: string;
+// }
+
+// interface NewRoom {
+//   name: string;
+//   description: string;
+// }
+
+// const Home = () => {
+//   const { user, isLoading, client, setCall } = useUser();
+//   const navigate = useNavigate();
+//   const [rooms, setRooms] = useState<Room[]>([]);
+
+//   const [newRoom, setNewRoom] = useState<NewRoom>({
+//     name: "",
+//     description: "",
+//   });
+
+//   const hashRoomName = (roomName: string): string => {
+//     const hash = CryptoJS.SHA256(roomName).toString(CryptoJS.enc.Base64);
+//     return hash.replace(/[^a-zA-Z0-9_-]/g, "");
+//   };
+
+//   const createRoom = async () => {
+//     const { name, description } = newRoom;
+//     if (!client || !user || !name || !description) return;
+//     const roomID = hashRoomName(name);
+//     const call = client.call("audio_room", roomID);
+//     await call.join({
+//       create: true,
+//       data: {
+//         members: [{ user_id: user.username }],
+//         custom: {
+//           title: name,
+//           description,
+//         },
+//       },
+//     });
+//     setCall(call);
+//     navigate(`/room/${roomID}`);
+//   };
+
+//   const joinRoom = async (roomId: string) => {
+//     const call = client?.call("audio_room", roomId);
+//     await call?.join();
+//     setCall(call);
+//     navigate("/room/" + roomId);
+//   };
+
+//   useEffect(() => {
+//     if (client) fetchListOfCalls();
+//   }, [client]);
+
+//   type CustomCallData = {
+//     description?: string;
+
+//     title?: string;
+//   };
+
+//   const fetchListOfCalls = async () => {
+//     const callsQueryResponse = await client?.queryCalls({
+//       filter_conditions: { ongoing: true },
+//       limit: 25,
+//       watch: true,
+//     });
+//     if (!callsQueryResponse) {
+//       console.log("Error fetching calls");
+//     } else {
+//       const getCallInfo = async (call: Call): Promise<Room> => {
+//         const callInfo = await call.get();
+//         const customData = callInfo.call.custom;
+//         const { title, description } = (customData || {}) as CustomCallData;
+//         const participantsLength = callInfo.members.length ?? 0;
+//         const createdBy = callInfo.call.created_by.name ?? "";
+//         const id = callInfo.call.id ?? "";
+//         return {
+//           id,
+//           title: title ?? "",
+//           description: description ?? "",
+//           participantsLength,
+//           createdBy,
+//         } as Room;
+//       };
+//       const roomPromises = await callsQueryResponse.calls.map((call) =>
+//         getCallInfo(call)
+//       );
+//       const rooms = await Promise.all(roomPromises);
+//       setRooms(rooms);
+//     }
+//   };
+
+//   if (isLoading) return <h1> ...</h1>;
+//   if (!isLoading && !user) {
+//     console.log(user);
+//     return <Navigate to="/sign-in" />;
+//   }
+
+//   if (!client) return;
+
+//   return (
+//     <StreamVideo client={client!}>
+//       <div className="min-h-screen flex flex-col md:flex-row">
+//         {/* <div className="flex-1  flex items-center justify-center p-10  bg-center">
+//           <h1>Welcome, {user?.name}</h1>
+
+//           <div className="form">
+//             <h2> Create Your Own Room</h2>
+//             <input
+//               placeholder="Room Name..."
+//               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+//                 setNewRoom((prev) => ({ ...prev, name: e.target.value }))
+//               }
+//             />
+//             <input
+//               placeholder="Room Description..."
+//               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+//                 setNewRoom((prev) => ({ ...prev, description: e.target.value }))
+//               }
+//             />
+//             <button
+//               onClick={createRoom}
+//               style={{ backgroundColor: "rgb(125, 7, 236)" }}
+//             >
+//               {" "}
+//               Create Room
+//             </button>
+//             <h2>Available Rooms</h2>
+//           </div>
+//         </div> */}
+
+//         <div className="flex-1 flex items-center justify-center p-10 bg-center  bg-black">
+//           <div className="max-w-lg w-full bg-white rounded-lg shadow-lg p-8">
+//             <h1 className="text-2xl font-bold text-center mb-6">
+//               Welcome, {user?.name}
+//             </h1>
+
+//             <div className="form space-y-6">
+//               <h2 className="text-xl font-semibold text-gray-700">
+//                 Create Your Own Room
+//               </h2>
+//               <input
+//                 placeholder="Room Name..."
+//                 className="w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+//                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+//                   setNewRoom((prev) => ({ ...prev, name: e.target.value }))
+//                 }
+//               />
+//               <input
+//                 placeholder="Room Description..."
+//                 className="w-full p-3 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+//                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+//                   setNewRoom((prev) => ({
+//                     ...prev,
+//                     description: e.target.value,
+//                   }))
+//                 }
+//               />
+//               <button
+//                 onClick={createRoom}
+//                 className="w-full py-3 bg-indigo-600 text-white font-bold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+//               >
+//                 Create Room
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="flex-1  bg-gray-100 p-10 ">
+//           <h2 className="text-xl font-semibold text-gray-700 mt-6 ">
+//             Available Rooms
+//           </h2>
+//           {rooms.map((room) => (
+//             // <div
+//             //   className="card bg-green-500 p-8 rounded-md text-center cursor-pointer"
+//             //   key={room.id}
+//             //   onClick={() => joinRoom(room.id)}
+//             // >
+//             //   <h4 className="text-white font-bold text-xl absolute top-24 m-2 bg-black p-1 px-5 rounded-xl">
+//             //     {room.title}
+//             //   </h4>
+//             //   <p>{room.description}</p>
+//             //   <p> {room.participantsLength} Participants</p>
+//             //   <p> Created By: {room.createdBy}</p>
+//             //   <div className="shine"></div>
+//             // </div>
+
+//             <div
+//               className="bg-card p-4 rounded-lg shadow-lg max-w-md mx-auto cursor-pointer"
+//               key={room.id}
+//               onClick={() => joinRoom(room.id)}
+//             >
+//               <div className="flex justify-between items-center">
+//                 <div>
+//                   <h2 className="text-xl font-semibold text-foreground">
+//                     {room.title}
+//                   </h2>
+//                   <p className="text-sm text-muted-foreground">
+//                     {room.description}
+//                   </p>
+//                 </div>
+//                 <img
+//                   alt="mic-icon"
+//                   className="w-6 h-6"
+//                   src="https://openui.fly.dev/openui/24x24.svg?text=🎤"
+//                 />
+//               </div>
+//               <div className="mt-4 flex justify-between items-center">
+//                 <div className="flex items-center">
+//                   {/* <img
+//                     src="https://placehold.co/40x40"
+//                     alt="Profile picture"
+//                     className="w-10 h-10 rounded-full border-2 border-primary"
+//                   /> */}
+//                   <BsPeopleFill className="w-10 h-10 rounded-full border-2 border-green-600" />
+
+//                   <div className="ml-2">
+//                     <p className="text-sm text-foreground">{room.createdBy}</p>
+//                     <p className="text-xs text-muted-foreground">Host</p>
+//                   </div>
+//                 </div>
+//                 <div className=" bg-black text-gray-300 rounded-full px-2 py-1">
+//                   <span className="text-sm font-semibold">
+//                     {room.participantsLength} Participants
+//                   </span>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </StreamVideo>
+//   );
+// };
+
+// export default Home;
+
 import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import "../../App.css";
@@ -25,11 +274,11 @@ const Home = () => {
   const { user, isLoading, client, setCall } = useUser();
   const navigate = useNavigate();
   const [rooms, setRooms] = useState<Room[]>([]);
-
   const [newRoom, setNewRoom] = useState<NewRoom>({
     name: "",
     description: "",
   });
+  const [creatingRoom, setCreatingRoom] = useState(false);
 
   const hashRoomName = (roomName: string): string => {
     const hash = CryptoJS.SHA256(roomName).toString(CryptoJS.enc.Base64);
@@ -39,6 +288,7 @@ const Home = () => {
   const createRoom = async () => {
     const { name, description } = newRoom;
     if (!client || !user || !name || !description) return;
+    setCreatingRoom(true);
     const roomID = hashRoomName(name);
     const call = client.call("audio_room", roomID);
     await call.join({
@@ -52,6 +302,7 @@ const Home = () => {
       },
     });
     setCall(call);
+    setCreatingRoom(false);
     navigate(`/room/${roomID}`);
   };
 
@@ -68,7 +319,6 @@ const Home = () => {
 
   type CustomCallData = {
     description?: string;
-
     title?: string;
   };
 
@@ -115,35 +365,7 @@ const Home = () => {
   return (
     <StreamVideo client={client!}>
       <div className="min-h-screen flex flex-col md:flex-row">
-        {/* <div className="flex-1  flex items-center justify-center p-10  bg-center">
-          <h1>Welcome, {user?.name}</h1>
-
-          <div className="form">
-            <h2> Create Your Own Room</h2>
-            <input
-              placeholder="Room Name..."
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setNewRoom((prev) => ({ ...prev, name: e.target.value }))
-              }
-            />
-            <input
-              placeholder="Room Description..."
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setNewRoom((prev) => ({ ...prev, description: e.target.value }))
-              }
-            />
-            <button
-              onClick={createRoom}
-              style={{ backgroundColor: "rgb(125, 7, 236)" }}
-            >
-              {" "}
-              Create Room
-            </button>
-            <h2>Available Rooms</h2>
-          </div>
-        </div> */}
-
-        <div className="flex-1 flex items-center justify-center p-10 bg-center  bg-black">
+        <div className="flex-1 flex items-center justify-center p-10 bg-center bg-black">
           <div className="max-w-lg w-full bg-white rounded-lg shadow-lg p-8">
             <h1 className="text-2xl font-bold text-center mb-6">
               Welcome, {user?.name}
@@ -173,32 +395,19 @@ const Home = () => {
               <button
                 onClick={createRoom}
                 className="w-full py-3 bg-indigo-600 text-white font-bold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                disabled={creatingRoom}
               >
-                Create Room
+                {creatingRoom ? "Creating Room..." : "Create Room"}
               </button>
             </div>
           </div>
         </div>
 
-        <div className="flex-1  bg-gray-100 p-10 ">
-          <h2 className="text-xl font-semibold text-gray-700 mt-6 ">
-            Available Rooms
+        <div className="flex-1 bg-gray-100 p-10">
+          <h2 className="text-xl font-semibold text-gray-700 mt-6">
+            {rooms.length === 0 ? "No Rooms" : "Available Rooms"}
           </h2>
           {rooms.map((room) => (
-            // <div
-            //   className="card bg-green-500 p-8 rounded-md text-center cursor-pointer"
-            //   key={room.id}
-            //   onClick={() => joinRoom(room.id)}
-            // >
-            //   <h4 className="text-white font-bold text-xl absolute top-24 m-2 bg-black p-1 px-5 rounded-xl">
-            //     {room.title}
-            //   </h4>
-            //   <p>{room.description}</p>
-            //   <p> {room.participantsLength} Participants</p>
-            //   <p> Created By: {room.createdBy}</p>
-            //   <div className="shine"></div>
-            // </div>
-
             <div
               className="bg-card p-4 rounded-lg shadow-lg max-w-md mx-auto cursor-pointer"
               key={room.id}
@@ -221,13 +430,7 @@ const Home = () => {
               </div>
               <div className="mt-4 flex justify-between items-center">
                 <div className="flex items-center">
-                  {/* <img
-                    src="https://placehold.co/40x40"
-                    alt="Profile picture"
-                    className="w-10 h-10 rounded-full border-2 border-primary"
-                  /> */}
                   <BsPeopleFill className="w-10 h-10 rounded-full border-2 border-green-600" />
-
                   <div className="ml-2">
                     <p className="text-sm text-foreground">{room.createdBy}</p>
                     <p className="text-xs text-muted-foreground">Host</p>
--- client/src/pages/sign-in/index.tsx
+++ client/src/pages/sign-in/index.tsx
@@ -1,3 +1,192 @@
+// import * as yup from "yup";
+// import { useForm, SubmitHandler } from "react-hook-form";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import "../../App.css";
+// import { StreamVideoClient, User } from "@stream-io/video-react-sdk";
+// import { Navigate, useNavigate } from "react-router-dom";
+// import { useUser } from "../../user-context";
+// import Cookies from "universal-cookie";
+// import { PEOPLES_IMAGES } from "../../avatars";
+
+// interface FormValues {
+//   username: string;
+//   name: string;
+// }
+
+// export const SignIn = () => {
+//   const schema = yup.object().shape({
+//     username: yup
+//       .string()
+//       .matches(/^[a-zA-Z0-9_.@$]+$/, "Invalid username")
+//       .required("Username is required"),
+//     name: yup.string().required("Name is required"),
+//   });
+
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm<FormValues>({
+//     resolver: yupResolver(schema),
+//   });
+
+//   const cookies = new Cookies(null, { path: "/" });
+
+//   const { setUser, setClient, isLoading } = useUser();
+
+//   const navigate = useNavigate();
+
+//   const onSubmit: SubmitHandler<FormValues> = async (data) => {
+//     const { username, name } = data;
+
+//     // const response = await fetch("http://localhost:3001/auth/createUser", {
+//     const response = await fetch(
+//       "https://audionix.onrender.com/auth/createUser",
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           username,
+//           name,
+//           image:
+//             PEOPLES_IMAGES[Math.floor(Math.random() * PEOPLES_IMAGES.length)],
+//         }),
+//       }
+//     );
+
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch token");
+//     }
+
+//     const responseData = await response.json();
+//     console.log(responseData);
+//     const user: User = {
+//       id: username,
+//       name,
+//     };
+
+//     const myClient = new StreamVideoClient({
+//       apiKey: "grs7vtwsg6mr",
+//       user,
+//       token: responseData.token,
+//     });
+
+//     const expires = new Date();
+//     expires.setDate(expires.getDate() + 1);
+//     cookies.set("token", responseData.token, {
+//       expires,
+//     });
+//     cookies.set("username", username, {
+//       expires,
+//     });
+//     cookies.set("name", name, {
+//       expires,
+//     });
+//     setClient(myClient);
+//     setUser({ name, username });
+//     navigate("/");
+//   };
+
+//   if (cookies.get("username") && !isLoading) return <Navigate to="/" />;
+
+//   return (
+//     <div className="min-h-screen flex flex-col md:flex-row">
+//       <div
+//         className="flex-1 bgc flex items-center justify-center p-10 bg-cover bg-center"
+//         style={{
+//           backgroundImage:
+//             "url('https://images.pexels.com/photos/6953868/pexels-photo-6953868.jpeg?auto=compress&cs=tinysrgb&w=600')",
+//           backgroundBlendMode: "darken",
+//         }}
+//       >
+//         <div className="text-center max-w-md">
+//           <h1 className="text-3xl font-bold text-white mb-4">
+//             "Your Voice, Our World - Welcome to{" "}
+//             <span className="text-green-300">Audionix</span> "
+//           </h1>
+//           <p className="text-white mb-8 text-gray-300">
+//             Connect and converse in real-time worldwide. Host shows, join
+//             discussions, and let your voice be heard. Audionix – Your Voice, Our
+//             World.
+//           </p>
+//         </div>
+//       </div>
+
+//       {/* <div className="flex-1 flex items-center justify-center p-10 relative">
+//         <div className="max-w-md w-full">
+//           <h1>Welcome to Audionix 2.0</h1>
+//           <form onSubmit={handleSubmit(onSubmit)}>
+//             <div>
+//               <label>Username: </label>
+//               <input type="text" {...register("username")} />
+//               {errors.username && (
+//                 <p style={{ color: "red" }}>{errors.username.message}</p>
+//               )}
+//             </div>
+//             <div>
+//               <label>Name: </label>
+//               <input type="text" {...register("name")} />
+//               {errors.name && (
+//                 <p style={{ color: "red" }}>{errors.name.message}</p>
+//               )}
+//             </div>
+//             <button type="submit">Sign In</button>
+//           </form>
+//         </div>
+//       </div> */}
+
+//       <div className="flex-1 flex items-center justify-center bg-gray-100 p-10">
+//         <div className="max-w-md w-full bg-white rounded-lg shadow-md p-8">
+//           <h1 className="text-3xl font-bold text-center mb-6">
+//             Welcome to Audionix 2.0
+//           </h1>
+//           <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+//             <div>
+//               <label className="block text-sm font-medium text-gray-700">
+//                 Username:
+//               </label>
+//               <input
+//                 type="text"
+//                 {...register("username")}
+//                 className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+//               />
+//               {errors.username && (
+//                 <p className="mt-2 text-sm text-red-600">
+//                   {errors.username.message}
+//                 </p>
+//               )}
+//             </div>
+//             <div>
+//               <label className="block text-sm font-medium text-gray-700">
+//                 Name:
+//               </label>
+//               <input
+//                 type="text"
+//                 {...register("name")}
+//                 className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+//               />
+//               {errors.name && (
+//                 <p className="mt-2 text-sm text-red-600">
+//                   {errors.name.message}
+//                 </p>
+//               )}
+//             </div>
+//             <button
+//               type="submit"
+//               className="w-full py-2 px-4 bg-indigo-600 text-white font-bold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+//             >
+//               Sign In
+//             </button>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+import { useState } from "react";
 import * as yup from "yup";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -36,57 +225,65 @@ export const SignIn = () => {
 
   const navigate = useNavigate();
 
+  const [loading, setLoading] = useState(false);
+
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    setLoading(true);
     const { username, name } = data;
 
-    // const response = await fetch("http://localhost:3001/auth/createUser", {
-    const response = await fetch(
-      "https://audionix.onrender.com/auth/createUser",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username,
-          name,
-          image:
-            PEOPLES_IMAGES[Math.floor(Math.random() * PEOPLES_IMAGES.length)],
-        }),
+    try {
+      const response = await fetch(
+        "https://audionix.onrender.com/auth/createUser",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            name,
+            image:
+              PEOPLES_IMAGES[Math.floor(Math.random() * PEOPLES_IMAGES.length)],
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch token");
       }
-    );
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch token");
+      const responseData = await response.json();
+      console.log(responseData);
+      const user: User = {
+        id: username,
+        name,
+      };
+
+      const myClient = new StreamVideoClient({
+        apiKey: "grs7vtwsg6mr",
+        user,
+        token: responseData.token,
+      });
+
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 1);
+      cookies.set("token", responseData.token, {
+        expires,
+      });
+      cookies.set("username", username, {
+        expires,
+      });
+      cookies.set("name", name, {
+        expires,
+      });
+      setClient(myClient);
+      setUser({ name, username });
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-
-    const responseData = await response.json();
-    console.log(responseData);
-    const user: User = {
-      id: username,
-      name,
-    };
-
-    const myClient = new StreamVideoClient({
-      apiKey: "grs7vtwsg6mr",
-      user,
-      token: responseData.token,
-    });
-
-    const expires = new Date();
-    expires.setDate(expires.getDate() + 1);
-    cookies.set("token", responseData.token, {
-      expires,
-    });
-    cookies.set("username", username, {
-      expires,
-    });
-    cookies.set("name", name, {
-      expires,
-    });
-    setClient(myClient);
-    setUser({ name, username });
-    navigate("/");
   };
 
   if (cookies.get("username") && !isLoading) return <Navigate to="/" />;
@@ -113,29 +310,6 @@ export const SignIn = () => {
           </p>
         </div>
       </div>
-
-      {/* <div className="flex-1 flex items-center justify-center p-10 relative">
-        <div className="max-w-md w-full">
-          <h1>Welcome to Audionix 2.0</h1>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <label>Username: </label>
-              <input type="text" {...register("username")} />
-              {errors.username && (
-                <p style={{ color: "red" }}>{errors.username.message}</p>
-              )}
-            </div>
-            <div>
-              <label>Name: </label>
-              <input type="text" {...register("name")} />
-              {errors.name && (
-                <p style={{ color: "red" }}>{errors.name.message}</p>
-              )}
-            </div>
-            <button type="submit">Sign In</button>
-          </form>
-        </div>
-      </div> */}
 
       <div className="flex-1 flex items-center justify-center bg-gray-100 p-10">
         <div className="max-w-md w-full bg-white rounded-lg shadow-md p-8">
@@ -176,8 +350,9 @@ export const SignIn = () => {
             <button
               type="submit"
               className="w-full py-2 px-4 bg-indigo-600 text-white font-bold rounded-md shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              disabled={loading}
             >
-              Sign In
+              {loading ? "Please wait..." : "Sign In"}
             </button>
           </form>
         </div>
